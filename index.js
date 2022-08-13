15893e26c860764cbd54f0912c0199ad41c6ef97
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2022-08-13 17:11:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+console.log('Hello B39WETamil');
+console.log("Second hello");
+
+
+// console.log(document);
+
+// alert("hey there !!!, I am browser JS");
+
+
+// Javascript
+
+// Datatypes
+
+// Primitive --> string, number, boolean(true/false), undefined --> false, null --> false
+// keywords --> reserved words which cannot be used for other purpose
+var name_str = '1065'; // String
+
+name_str = '1000';
+
+// console.log(typeof name_str);
+// console.log(name_str);
+
+// console.log(name_str.split(''));
+
+var age = 16;
+// console.log(typeof age);
+// console.log(age);
+
+// console.log(typeof true);// boolean true / false
+
+// Comparison Operator
+// +, -, /, * ---> arithematice operator
+// logical, & --> and | --> or, ! --> not
+// >, <, >=, <=, ==, != 
+
+// if-else
+if ( age % 2 == 0 ) {
+  console.log(age % 2 != 0);
+  console.log("Your age is even");
+} else {
+  console.log("Your age is odd");
+}
+
+// Print the following
+// 1 x 1 = 1
+// 2 x 2 = 4
+// .
+// .
+// 5 x 5 = 25
+
+// loop --> doing something for certain times
+// i = 1 ----> 1 x 1 = 1
+// i = 2 ----> 2 x 2 = 4
+
+// Print 1, 4, 9, 16, 25
+
+// for loop syntax iteration ---> looping
+// for (var i=1; i<=5; i++) {
+//   console.log( i + ' x ' + i + ' = ' + (i * i)); 
+// }
+
+var y = '13';
+var z = 13;
+
+
+console.log(z * y);
+
+var x = '';
+for (var i = 1; i < 6; i = i + 1) {
+  console.log(x);
+  x = x + (i * i) + " ";
+}
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Composite --> 
+
+
+
---
+++ sample-node.js
@@ -0,0 +1,6 @@
+console.log('Hello B39WETamil');
+console.log("Second hello");
+
+
+alert("hey there !!!, I am browser JS");
+
